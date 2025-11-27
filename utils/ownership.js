3f95ce8769a9388ef48d22e3a18d9bb4e208187b
Kaming <lokaming86@gmail.com>
2025-11-27 23:58:49 +0700

    Fixing the ownership issues

--- utils/ownership.js
+++ utils/ownership.js
@@ -2,25 +2,38 @@ import { User } from "../models/userModel.js";
 import { Polis } from "../models/polisModel.js";
 import { Pembayaran } from "../models/pembayaranModel.js"
 import { Klaim } from "../models/klaimModel.js";
-
 export const getOwnerIdFromResource = async (modelName, id) => {
   switch (modelName) {
     case "User": {
       const user = await User.findById(id);
       return user?._id;
     }
+
     case "Polis": {
-      const polis = await Polis.findById(id);
-      return polis?.user._id;
+      const polis = await Polis.findById(id).populate("userId", "_id");
+      return polis?.userId?._id;
     }
+
     case "Pembayaran": {
-      const pembayaran = await Pembayaran.findById(id).populate("polis", "user");
-      return pembayaran?.polis?.user._id;
+      const pembayaran = await Pembayaran.findById(id)
+        .populate({
+          path: "polis",
+          populate: { path: "userId", select: "_id" }
+        });
+
+      return pembayaran?.polis?.userId?._id;
     }
+
     case "Klaim": {
-      const klaim = await Klaim.findById(id).populate("polis", "user");
-      return klaim?.polis?.user._id;
+      const klaim = await Klaim.findById(id)
+        .populate({
+          path: "polis",
+          populate: { path: "userId", select: "_id" }
+        });
+
+      return klaim?.polis?.userId?._id;
     }
+
     default:
       throw new Error("Model tidak dikenali untuk ownership");
   }
